d26fda8c040e9df26f5a0d6718f16b2cb5a739a6
Leandro Fischer <fischer476@gmail.com>
2022-02-03 22:22:16 -0300

    teste de commit através de outro repo

--- codigo.py
+++ codigo.py
@@ -3,3 +3,4 @@ print('git')
 print('alterado') #alteração para teste no GIT
 print('quarta linha e quinto commit') #atualização vindo do github
 print('quinta linha') #testando a sincronização do GIT com o local
+print('comportamento do GIT em outro repositorio') #testando clone de repositorios
